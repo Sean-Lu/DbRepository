b2f88942751ed260b34f8675a48d85d3e9346851
Lu Weixian <luwx@newgrand.cn>
2022-01-12 11:20:02 +0800

    demo

--- examples/NetCore/Example.Domain/Extensions/ServiceCollectionExtensions.cs
+++ examples/NetCore/Example.Domain/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,11 @@
-﻿using System.Reflection;
+﻿using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Reflection;
 using Example.Infrastructure.Extensions;
 using Example.Infrastructure.Impls;
 using Microsoft.Extensions.DependencyInjection;
 using MySql.Data.MySqlClient;
+using Oracle.ManagedDataAccess.Client;
 using Sean.Core.DbRepository;
 using Sean.Core.DbRepository.Config;
 using Sean.Core.DbRepository.Extensions;
@@ -19,7 +22,11 @@ namespace Example.Domain.Extensions
         public static void AddDomainDI(this IServiceCollection services)
         {
             services.RegisterServicesByAssemblyInterface(Assembly.GetExecutingAssembly(), "Repository", ServiceLifetime.Transient);
+
             DatabaseType.MySql.SetDbProviderMap(new DbProviderMap("MySql.Data.MySqlClient", MySqlClientFactory.Instance));
+            DatabaseType.SqlServer.SetDbProviderMap(new DbProviderMap("System.Data.SqlClient", SqlClientFactory.Instance));
+            DatabaseType.Oracle.SetDbProviderMap(new DbProviderMap("Oracle.ManagedDataAccess.Client", OracleClientFactory.Instance));
+            DatabaseType.SQLite.SetDbProviderMap(new DbProviderMap("System.Data.SQLite", SQLiteFactory.Instance));
         }
     }
 }
