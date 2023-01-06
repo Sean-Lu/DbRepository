4bd8b7b66d46e252b4c1caaf3e82373414a8421a
Lu Weixian <luwx@newgrand.cn>
2023-01-07 01:07:07 +0800

    Table<TEntity>

--- src/Sean.Core.DbRepository/Table.cs
+++ src/Sean.Core.DbRepository/Table.cs
@@ -7,19 +7,25 @@ namespace Sean.Core.DbRepository
 {
     public static class Table<TEntity> where TEntity : class
     {
-        public static string Field(Expression<Func<TEntity, object>> fieldExpression)
+        public static string TableName(Func<string, string> tableNameFactory = null)
         {
-            return fieldExpression.GetFieldNames()?.FirstOrDefault();
+            var tableName = typeof(TEntity).GetMainTableName();
+            return tableNameFactory != null ? tableNameFactory(tableName) : tableName;
+        }
+
+        public static string Field(Expression<Func<TEntity, object>> fieldExpression, string alias = null)
+        {
+            return !string.IsNullOrWhiteSpace(alias) ? $"{fieldExpression.GetFieldNames()?.FirstOrDefault()} AS {alias}" : fieldExpression.GetFieldNames()?.FirstOrDefault();
+        }
+
+        public static string FieldWithTableName(Expression<Func<TEntity, object>> fieldExpression, string alias = null, Func<string, string> tableNameFactory = null)
+        {
+            return $"{TableName(tableNameFactory)}.{Field(fieldExpression, alias)}";
         }
 
         public static string[] Fields(Expression<Func<TEntity, object>> fieldExpression)
         {
             return fieldExpression.GetFieldNames()?.ToArray();
-        }
-
-        public static string TableName()
-        {
-            return typeof(TEntity).GetMainTableName();
         }
 
         public static string SqlWhereClause(DatabaseType databaseType, Expression<Func<TEntity, bool>> whereExpression)
