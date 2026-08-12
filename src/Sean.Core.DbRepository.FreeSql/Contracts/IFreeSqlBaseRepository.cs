50406806a09958a4b025af2502adf3656212f1c6
Lu Weixian <weixian.lu@foxmail.com>
2026-08-13 02:14:51 +0800

    chore: 删除不再维护的 ORM 适配项目

--- src/Sean.Core.DbRepository.FreeSql/Contracts/IFreeSqlBaseRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using FreeSql;
-
-namespace Sean.Core.DbRepository.FreeSql;
-
-public interface IFreeSqlBaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
-{
-
-}
--- src/Sean.Core.DbRepository.SqlSugar/Contracts/ISqlSugarBaseRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using SqlSugar;
-
-namespace Sean.Core.DbRepository.SqlSugar;
-
-public interface ISqlSugarBaseRepository<TEntity> : ISugarRepository, ISimpleClient<TEntity> where TEntity : class, new()
-{
-
-}
